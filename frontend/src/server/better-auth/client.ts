6eca205b61875d57526297ce9925401f1132207d
khaironnahar4 <khaironnahar247@gmail.com>
2026-01-21 11:14:19 +0600

    did the next.js setup

--- frontend/src/server/better-auth/client.ts
+++
@@ -1,8 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-import { env } from "~/env";
-
-export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
-});
-
-export type Session = typeof authClient.$Infer.Session;
